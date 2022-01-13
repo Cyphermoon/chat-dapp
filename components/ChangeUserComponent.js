8c8de81e8dfff4cb7deea83f4a14aeaac6af977e
Cypher Moon <temperaturecaleb@gmail.com>
2022-01-13 22:54:04 +0100

    implemented the change user modal

--- components/ChangeUserComponent.js
+++ components/ChangeUserComponent.js
@@ -1,27 +1,28 @@
+import { useState } from "react";
 import { useMoralis } from "react-moralis";
+import ChangeUserNameModal from "./ChangeUserNameModal";
 
 const ChangeUserComponent = () => {
-  const { setUserData, isUserUpdating } = useMoralis();
+  const { isUserUpdating } = useMoralis();
+  const [isModalDisplayed, setIsModalDisplayed] = useState(false);
 
-  const setUserName = () => {
-    const username = prompt("Enter your new username: ");
-
-    if (!username) return;
-
-    setUserData({
-      username,
-    });
+  const toggleIsModalDisplayed = () => {
+    setIsModalDisplayed(!isModalDisplayed);
   };
 
   return (
     <div className="text-sm absolute top-5 right-5">
       <button
-        onClick={setUserName}
+        onClick={toggleIsModalDisplayed}
         disabled={isUserUpdating}
         className="text-black hover:focus:active:text-pink-600 bg-yellow-600 rounded-sm p-2"
       >
         Change username
       </button>
+      <ChangeUserNameModal
+        isModalDisplayed={isModalDisplayed}
+        toggleIsModalDisplayed={toggleIsModalDisplayed}
+      />
     </div>
   );
 };
